2c4c9f15370e1279b44efc9660c137ca4f22db2f
Phazer <jurgen.hannus@gmail.com>
2014-09-12 00:05:13 +0300

    stripped down

--- src/main/java/org/springframework/samples/petclinic/service/ClinicService.java
+++ src/main/java/org/springframework/samples/petclinic/service/ClinicService.java
@@ -15,14 +15,10 @@
  */
 package org.springframework.samples.petclinic.service;
 
-import java.util.Collection;
-
 import org.springframework.dao.DataAccessException;
-import org.springframework.samples.petclinic.model.Owner;
-import org.springframework.samples.petclinic.model.Pet;
-import org.springframework.samples.petclinic.model.PetType;
 import org.springframework.samples.petclinic.model.Vet;
-import org.springframework.samples.petclinic.model.Visit;
+
+import java.util.Collection;
 
 
 /**
@@ -32,20 +28,5 @@ import org.springframework.samples.petclinic.model.Visit;
  */
 public interface ClinicService {
 
-    public Collection<PetType> findPetTypes() throws DataAccessException;
-
-    public Owner findOwnerById(int id) throws DataAccessException;
-
-    public Pet findPetById(int id) throws DataAccessException;
-
-    public void savePet(Pet pet) throws DataAccessException;
-
-    public void saveVisit(Visit visit) throws DataAccessException;
-
     public Collection<Vet> findVets() throws DataAccessException;
-
-    public void saveOwner(Owner owner) throws DataAccessException;
-
-    Collection<Owner> findOwnerByLastName(String lastName) throws DataAccessException;
-
 }
